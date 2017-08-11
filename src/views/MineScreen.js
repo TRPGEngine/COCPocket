cc603093a4b4c7a6bd868e5d673774a65fed734e
moonrailgun <moonrailgun@gmail.com>
2017-08-11 17:20:38 +0800

    退出登录按钮

--- src/views/MineScreen.js
+++ src/views/MineScreen.js
@@ -1,5 +1,5 @@
 const React = require('react');
-const { StyleSheet, ScrollView, Button } = require('react-native');
+const { StyleSheet, ScrollView, TouchableOpacity, Text, Button } = require('react-native');
 const { connect } = require('react-redux');
 const sb = require('react-native-style-block');
 const color = require('../util/color');
@@ -7,12 +7,19 @@ const Avatar = require('./components/mine/Avatar');
 const List = require('./components/mine/List');
 
 class MineScreen extends React.Component {
+  _logout() {
+    alert('logout');
+  }
+
   render() {
     const { isLoggedIn } = this.props;
     return (
       <ScrollView style={styles.container}>
         <Avatar />
         <List />
+        <TouchableOpacity style={styles.logoutBtn} onPress={this._logout}>
+          <Text style={[sb.color('#FFFFFF'),sb.font(16)]}>退出登录</Text>
+        </TouchableOpacity>
       </ScrollView>
     )
   }
@@ -31,14 +38,20 @@ MineScreen.navigationOptions = props => {
   }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     // justifyContent: 'center',
     // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-});
+  logoutBtn: [
+    sb.bgColor('red'),
+    sb.size(null, 44),
+    sb.center(),
+    sb.margin(10,0,0,0),
+  ]
+};
 
 const mapStateToProps = state => ({
   isLoggedIn: state.auth.isLoggedIn,
